a72e64788123ba540be66ad353cd936ef98e73ac
TajanaSA <tajana.salkic@skyline.be>
2025-09-10 10:34:16 +0200

    Added a class for Transport Sreams, Transport Stream and Service

---
+++ QAction_3/TransportStreams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Skyline.DataMiner.Scripting.Parameter;
+
+namespace QAction_3
+{
+	public class TransportStreams
+	{
+		public List<Transport_Stream> transport_streams { get; set; }
+	}
+
+	public class Transport_Stream
+	{
+		public int ts_id { get; set; }
+
+		public string ts_name { get; set; }
+
+		public string multicast { get; set; }
+
+		public string sourceIp { get; set; }
+
+		public int network_id { get; set; }
+
+		public List<Service> services { get; set; }
+	}
+
+	public class Service
+	{
+		public int service_id { get; set; }
+
+		public string service_name { get; set; }
+
+		public string service_type { get; set; }
+
+		public string service_provider { get; set; }
+	}
+
+}

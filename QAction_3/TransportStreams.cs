cfefef74b33d3a9df69c8d7a7611cb7aae542b1b
TajanaSA <tajana.salkic@skyline.be>
2025-09-10 12:04:30 +0200

    Added JSON deserialization and filled the Transport Streams Table with Data in QAction_3 that is triggered with Poll Data button

--- QAction_3/TransportStreams.cs
+++ QAction_3/TransportStreams.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 using static Skyline.DataMiner.Scripting.Parameter;
@@ -10,32 +11,43 @@ namespace QAction_3
 {
 	public class TransportStreams
 	{
+		[JsonPropertyName("transport_streams")]
 		public List<Transport_Stream> transport_streams { get; set; }
 	}
 
 	public class Transport_Stream
 	{
+		[JsonPropertyName("ts_id")]
 		public int ts_id { get; set; }
 
+		[JsonPropertyName("ts_name")]
 		public string ts_name { get; set; }
 
+		[JsonPropertyName("multicast")]
 		public string multicast { get; set; }
 
+		[JsonPropertyName("sourceIp")]
 		public string sourceIp { get; set; }
 
+		[JsonPropertyName("network_id")]
 		public int network_id { get; set; }
 
+		[JsonPropertyName("services")]
 		public List<Service> services { get; set; }
 	}
 
 	public class Service
 	{
+		[JsonPropertyName("service_id")] 
 		public int service_id { get; set; }
 
+		[JsonPropertyName("service_name")]
 		public string service_name { get; set; }
 
+		[JsonPropertyName("service_type")]
 		public string service_type { get; set; }
 
+		[JsonPropertyName("service_provider")]
 		public string service_provider { get; set; }
 	}
 

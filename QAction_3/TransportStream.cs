ccda4fae6cbd2e6095a34940f0d54df3abf2e539
emirburic1 <emir.buric@skyline.be>
2025-09-12 15:43:11 +0200

    Created the QAction and added Service and TransportStream classes

---
+++ QAction_3/TransportStream.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QAction_3
+{
+    public class TransportStreams
+    {
+        public List<TransportStream> TransportStreamsList { get; set; }
+    }
+
+
+    public class TransportStream
+    {
+        [JsonProperty("ts_id")]
+        public int TransportStreamId { get; set; }
+
+        [JsonProperty("ts_name")]
+        public string Name { get; set; }
+
+        [JsonProperty("multicast")]
+        public string Multicast { get; set; }
+
+        [JsonProperty("sourceIp")]
+        public string SourceIp { get; set; }
+
+        [JsonProperty("network_id")]
+        public int NetworkId { get; set; }
+
+        [JsonProperty("services")]
+        public List<Service> Services { get; set; }
+    }
+}
